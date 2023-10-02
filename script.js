bffe35f966e25d2b2f6ebeaca8f138f11d64f47d
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-02 10:28:27 +0000

    Create if statement

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+mode will enable us to switch between the dark, light, and ninja modes that define the look of the UI.
+color stores the background color for the selected mode.
+image stores the path to the image that'll display for the selected mode.
+label informs the user what mode they're currently using.
+
+Assemble an if statement that checks if the mode variable is set to "dark". Leave its code block empty for now.
+
+*/
+
+
+let mode = "light";
+let color = "ghostWhite";
+let image = "/img/light-icon.png";
+let label = "Light Mode";
+
+if (mode === "dark") {
+
+}
